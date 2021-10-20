860b51d1c0aea6e6287380fb8a86ac43213e4f65
yavuzkocar <87602474+yavuzkocar@users.noreply.github.com>
2021-10-21 02:15:53 +0300

    operatorler konusu

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         float a = Convert.ToInt32(Console.ReadLine());
+         float b = Convert.ToInt32(Console.ReadLine());
+         float c = Convert.ToInt32(Console.ReadLine());
+         float d = a*b/100;
+         float e = a*c/100;
+         float sonuc1 = a+d+e;
+         object h = Math.Round(sonuc1);
+
+         Console.WriteLine(h);
+
+        }
+    }
+}
